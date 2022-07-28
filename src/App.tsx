3c27ecc4ad6efcdf104799fe1b40f0e4c99a98c1
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-07-28 13:21:06 -0400

    fix: safari viewport improvement

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Nav from "./components/Nav";
 
 function App() {
   return (
-    <main className="grid grid-rows-[repeat(10,_1fr)] bg-gray-100 w-screen h-screen">
+    <main className="grid grid-rows-[repeat(10,_1fr)] bg-gray-100 w-screen h-full">
       <Nav />
       <Main />
     </main>
